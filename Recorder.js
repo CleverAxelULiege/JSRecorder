f3f596e00aac4529aa624b1a123f24842a4e62ca
A Clever <aclever@uliege.be>
2024-01-12 11:44:34 +0100

    first commit

---
+++ Recorder.js
@@ -0,0 +1,111 @@
+export class Recorder {
+    /**
+     * @param {HTMLButtonElement} startRecordingButton 
+     * @param {HTMLButtonElement} stopRecordingButton 
+     * @param {HTMLVideoElement} previewVideo 
+     * @param {HTMLVideoElement} recordedVideo 
+     */
+    constructor(
+        startRecordingButton,
+        stopRecordingButton,
+        previewVideo,
+        recordedVideo,
+        constraints = { video: true, audio: true }
+    ) {
+        /**@type {HTMLButtonElement} */
+        this.startRecordingButton = startRecordingButton;
+        /**@type {HTMLButtonElement} */
+        this.stopRecordingButton = stopRecordingButton;
+        /**@type {HTMLVideoElement} */
+        this.previewVideo = previewVideo;
+        /**@type {HTMLVideoElement} */
+        this.recordedVideo = recordedVideo;
+
+
+        /**@type {{video:boolean, audio:boolean}} */
+        this.constraints = constraints;
+
+
+        /**@type {MediaStream|null} */
+        this.mediaStream = null;
+
+        /**@type {MediaRecorder|null} */
+        this.mediaRecorder = null;
+        /**@type {Blob[]} */
+        this.recordedChunks = [];
+
+        this.initEventListeners();
+    }
+
+
+    initEventListeners() {
+        this.startRecordingButton.addEventListener("click", () => {
+            this.asyncStartStreamingAndRecording();
+        });
+
+        this.stopRecordingButton.addEventListener("click", () => {
+            this.stopStreamingAndRecording();
+        })
+    }
+
+    async asyncStartStreamingAndRecording() {
+        await this.startStreamingToPreviewVideo();
+        this.startRecording();
+    }
+
+    /**@returns {Promise<void>} */
+    startStreamingToPreviewVideo() {
+        return new Promise((resolve, reject) => {
+            navigator.mediaDevices.getUserMedia(this.constraints)
+                .then((stream) => {
+                    this.mediaStream = stream;
+                    this.previewVideo.srcObject = this.mediaStream;
+                    resolve();
+                });
+        })
+    }
+
+    startRecording() {
+        this.mediaRecorder = new MediaRecorder(this.mediaStream);
+        this.initEventListenersOnMediaRecorder();
+        this.mediaRecorder.start();
+    }
+
+    initEventListenersOnMediaRecorder() {
+
+        this.mediaRecorder.ondataavailable = (blobEvent) => {
+            this.recordedChunks.push(blobEvent.data);
+        }
+
+        this.mediaRecorder.onstop = (event) => {
+            console.log("stopped the recording");
+            // let recordedBlob = new Blob(this.recordedChunks, { type: "video/webm" });
+            let recordedBlob = new Blob(this.recordedChunks, { type: "video/mp4" });
+            this.recordedVideo.src = URL.createObjectURL(recordedBlob);
+        }
+    }
+
+    stopStreamingAndRecording() {
+        if (this.mediaStream === null) {
+            window.alert("No media stream set, you probably didn't start the recording.");
+            return;
+        }
+        
+        if(this.mediaRecorder === null){
+            window.alert("No media recorder set, you probably didn't start the recording.");
+            return;
+        }
+
+        this.stopStreamingToPreviewVideo();
+        this.stopRecording();
+    }
+
+    stopStreamingToPreviewVideo() {
+        this.mediaStream.getVideoTracks()[0].enabled = false;
+        this.mediaStream.getAudioTracks()[0].enabled = false;
+    }
+
+    stopRecording() {
+        this.mediaRecorder.stop();
+    }
+}
---
+++ script.js
@@ -0,0 +1,13 @@
+import { Recorder } from "./Recorder.js";
+
+/** @type {HTMLButtonElement}*/
+const START_RECORDING_BUTTON = document.getElementById("start_recording_button");
+/** @type {HTMLButtonElement}*/
+const STOP_RECORDING_BUTTON = document.getElementById("stop_recording_button");
+
+/** @type {HTMLVideoElement}*/
+const PREVIEW_VIDEO = document.getElementById("preview_video");
+/** @type {HTMLVideoElement}*/
+const RECORDED_VIDEO = document.getElementById("recorded_video");
+
+let recorder = new Recorder(START_RECORDING_BUTTON, STOP_RECORDING_BUTTON, PREVIEW_VIDEO, RECORDED_VIDEO);
