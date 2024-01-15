8e2888d2f71c032f7ebafdef940cd9055146a75a
A Clever <aclever@mail.be>
2024-01-15 13:41:01 +0100

    Squashed commit of the following:
    commit bbe754bffe2bd261d94e3c92ccf2811da3c8298b
    Author: A Clever <aclever@mail.be>
    Date:   Mon Jan 15 13:40:07 2024 +0100
    
        close button recorder
    
    commit 16519fa7634c8e9548989ebe5bac1d68533753ba
    Author: A Clever <aclever@mail.be>
    Date:   Mon Jan 15 11:28:35 2024 +0100
    
        start front

---
+++ script_front.js
@@ -0,0 +1,40 @@
+const DISPLAY_RECORDER_BUTTON = document.querySelector("#display_recorder_button");
+
+//c'est le truc avec un background rgba avec une opacité de ~0.7 et qui contient le retour caméra, bouton etc
+const RECORDER_CONTAINER = document.querySelector(".recorder_container");
+const RECORDER = document.querySelector(".recorder");
+const CLOSE_RECORDER_BUTTON = document.querySelector(".close_recorder_button");
+
+let isRecorderContainerUp = false;
+
+window.addEventListener("click", (e) => {
+    if(!isRecorderContainerUp){
+        return;
+    }
+    
+    if(e.target.closest(".recorder") == null){
+        closeRecorderContainer();
+    }
+});
+
+CLOSE_RECORDER_BUTTON.addEventListener("click", () => {
+    closeRecorderContainer();
+})
+
+DISPLAY_RECORDER_BUTTON.addEventListener("click", () => {
+    RECORDER_CONTAINER.classList.remove("hidden");
+    document.body.style.overflowY = "hidden";
+    setTimeout(() => {
+        isRecorderContainerUp = true;
+        RECORDER.classList.remove("animation_enter_recorder");
+    });
+});
+
+
+function closeRecorderContainer(){
+    RECORDER_CONTAINER.classList.add("hidden");
+    document.body.style.overflowY = "";
+    isRecorderContainerUp = false;
+    RECORDER.classList.add("animation_enter_recorder");
+}
+
